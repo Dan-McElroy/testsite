587178bae7f6e73781f4d5deb1b3ac22db8ecdc1
Dan McElroy <daniel@mcelroys.net>
2021-02-26 10:04:41 +0100

    Fixed mislaid public directory and attempted to fix GraphQL connectivity.

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log(process.env.SERVER_API_URL);
-
 export const client = new ApolloClient({
   uri: process.env.SERVER_API_URL,
   cache: new InMemoryCache(),
